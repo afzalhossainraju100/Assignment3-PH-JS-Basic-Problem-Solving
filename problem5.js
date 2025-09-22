16561d316b3e3b7bf28e5151d86aadcf28ade290
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-09-22 18:50:34 +0600

    problem 5 solve

---
+++ problem5.js
@@ -0,0 +1,6 @@
+/** Problem 05 - ( PH Email Generator )  */
+var student = { name: "jhankar", roll: 1014, department: "cse" };
+//write your code here
+var student = { name: "monu", roll: 99, department: "eee" };
+var student = { name: "mewo", roll: 96, department: "cse" };
+console.log(`${student.name}${student.roll}.${student.department}@ph.ac.bd`);
