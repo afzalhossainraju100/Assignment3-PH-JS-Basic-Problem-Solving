21874f29d9b22400fc968a2d6083b9443aebc782
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-09-22 18:42:44 +0600

    p1 solve

---
+++ problem1.js
@@ -0,0 +1,11 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 800;
+//write your code here
+var area = 100;
+var area = 15;
+var area = 2060;
+let div;
+if (area > 0) {
+  div = area / 2;
+}
+console.log(div);
